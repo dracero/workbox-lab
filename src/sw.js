44e95015fba108f3d256fedeeda003c9319cb248
Glitch (dracero-workbox-lab) <none>
2019-10-08 19:23:55 +0000

    ⚽️🍳 Checkpoint ./src/sw.js:3726892/289

--- src/sw.js
+++ src/sw.js
@@ -23,3 +23,16 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+workbox.routing.registerRoute(
+  /(.*)articles(.*)\.(?:png|gif|jpg)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  })
+);
